771c145c1c421ede8ceb37b202a035f3f40f39b9
Mario Martin Alonso <marioma87@gmail.com>
2024-06-19 10:02:38 +0200

    doc: add READMEs and await methods info

--- pkg/await/init.go
+++ pkg/await/init.go
@@ -12,12 +12,17 @@ const (
 	TILT_AWAIT_DEBUGGER_ENABLED = "TILT_AWAIT_DEBUGGER_ENABLED"
 )
 
+// It checks if the TILT_AWAIT_DEBUGGER_ENABLED environment variable is set to true,
+// and if so, it starts the await functionality with the specified logger.
 func init() {
 	if isAwaitEnabled() {
 		await.Start(await.Logger(log.Printf))
 	}
 }
 
+// isAwaitEnabled is a helper function that checks if the TILT_AWAIT_DEBUGGER_ENABLED
+// environment variable is set to true.
+// It returns true if the variable is set to true, and false otherwise.
 func isAwaitEnabled() bool {
 	awaitDebuggerEnabled, exists := os.LookupEnv(TILT_AWAIT_DEBUGGER_ENABLED)
 	isAwaitEnabled, _ := strconv.ParseBool(awaitDebuggerEnabled)
